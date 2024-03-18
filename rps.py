1ffde9484fd08f562503ffeddd9ccdf3fe9e0400
sumana-sdet <v.sumana@gmail.com>
2024-03-17 18:03:05 -0700

    refactor to add recursion

--- rps.py
+++ rps.py
@@ -1,39 +1,45 @@
 import random
 import sys
 
-play_again = True
 
-while play_again:
-    print("Let's Play : ROCK PAPER SCISSORS\n")
-    players_choice = input("Enter....\n1 for Rock\n2 for Paper\n3 for Scissors \n")
+def play_rps():
+    players_choice = input("\nEnter....\n1 for Rock\n2 for Paper\n3 for Scissors \n")
 
     print("\nYour Choice : ", players_choice)
 
-    player = int(players_choice)
+    if players_choice not in ["1", "2", "3"]:
+        print("Your choice must be 1 , 2, or 3.")
+        return play_rps()
 
+    player = int(players_choice)
     computers_choice = random.choice("123")
     computer = int(computers_choice)
     print("Computers Choice : ", computers_choice)
 
-    if player < 1 or player > 3:
-        sys.exit("Your choice must be 1 , 2, or 3.")
-
     if player == 1 and computer == 3:
-        print(" 🎊 You WIN!")
+        print("\n 🎊 You WIN!")
     elif player == 2 and computer == 1:
-        print(" 🎊 You WIN!")
+        print("\n 🎊 You WIN!")
     elif player == 3 and computer == 2:
-        print(" 🎊 You WIN!")
+        print("\n 🎊 You WIN!")
     elif player == computer:
-        print(" 😮 TIE game !")
+        print("\n 😮 TIE game !")
     else:
-        print(" 🫥 Computer WINS!!!")
+        print("\n 🫥 Computer WINS!!!")
 
-    play_again = input("\n Play Again? \n Y for Yes \n Q for Quit\n\n")
-    if play_again.lower() == 'y':
-        continue
+    print("\nWant To Play Again?")
+    while True:
+        play_again = input("Y for Yes \nQ for Quit\n")
+        if play_again.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
+    if play_again.lower() == "y":
+        return play_rps()
     else:
-        print("Thank you for playing!!!!\n")
-        play_again = False
+        print("Thank you for playing!!!!")
+        sys.exit("\n👋 .. BYE")
 
-    sys.exit("👋 .. BYE")
+
+print("Let's Play : ROCK PAPER SCISSORS\n")
+play_rps()
