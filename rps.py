1b1c1ed81506e36e64c53b33ac2f9e0f05da3239
sumana-sdet <v.sumana@gmail.com>
2024-03-17 17:23:55 -0700

    modify to add loops

--- rps.py
+++ rps.py
@@ -1,28 +1,39 @@
 import random
 import sys
-print("Let's Play : ROCK PAPER SCISSORS")
-print("")
-players_choice = input("Enter....\n1 for Rock\n2 for Paper\n3 for Scissors \n\n")
-print("")
-print("")
-print("Your Choice : ", players_choice)
 
-player = int(players_choice)
+play_again = True
 
-computers_choice = random.choice("123")
-computer = int(computers_choice)
-print("Computers Choice : ", computers_choice)
+while play_again:
+    print("Let's Play : ROCK PAPER SCISSORS\n")
+    players_choice = input("Enter....\n1 for Rock\n2 for Paper\n3 for Scissors \n")
 
-if player < 1 or player > 3:
-    sys.exit("Your choice must be 1 , 2, or 3.")
+    print("\nYour Choice : ", players_choice)
 
-if player == 1 and computer == 3:
-    print(" 🎊 You WIN!")
-elif player == 2 and computer == 1:
-    print(" 🎊 You WIN!")
-elif player == 3 and computer == 2:
-    print(" 🎊 You WIN!")
-elif player == computer:
-    print(" 😮 TIE game !")
-else:
-    print(" 🫥 Computer WINS!!!")
+    player = int(players_choice)
+
+    computers_choice = random.choice("123")
+    computer = int(computers_choice)
+    print("Computers Choice : ", computers_choice)
+
+    if player < 1 or player > 3:
+        sys.exit("Your choice must be 1 , 2, or 3.")
+
+    if player == 1 and computer == 3:
+        print(" 🎊 You WIN!")
+    elif player == 2 and computer == 1:
+        print(" 🎊 You WIN!")
+    elif player == 3 and computer == 2:
+        print(" 🎊 You WIN!")
+    elif player == computer:
+        print(" 😮 TIE game !")
+    else:
+        print(" 🫥 Computer WINS!!!")
+
+    play_again = input("\n Play Again? \n Y for Yes \n Q for Quit\n\n")
+    if play_again.lower() == 'y':
+        continue
+    else:
+        print("Thank you for playing!!!!\n")
+        play_again = False
+
+    sys.exit("👋 .. BYE")
